1591f322d42967c7abe0a9771c2a3998ba1a3b7f
ayoakpata <akpata.ayobami@gmail.com>
2020-02-12 23:26:51 +0100

    workflow fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist/assets"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-    publicPath: "/assets/"
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/'
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
         }
       }
-    ]
+    }]
   }
 };
